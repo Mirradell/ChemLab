a318775e4bf16f623266672f96e3079130fbfb81
Mirradell <olya.shtejn@yandex.ru>
2021-03-31 12:12:57 +0300

    created project

---
+++ Lab/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    Rigidbody capsule;
+    // Start is called before the first frame update
+    void Start()
+    {
+        capsule = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");// * 30;
+        var vertical = Input.GetAxis("Vertical");// * 20;
+
+        if (horizontal != 0 || vertical != 0) // Движение камеры пострелочкам вправо/влево/вверх/вниз
+        {
+            var yNew = transform.localEulerAngles.y + horizontal;
+            if (yNew < 0)
+                yNew += 360;
+            if (yNew < 340 && yNew > 180)
+                yNew = 340;
+            else if (yNew > 30 && yNew <= 180)
+                yNew = 30;
+            
+            transform.localEulerAngles = new Vector3(Mathf.Clamp(transform.localEulerAngles.x - vertical, 0f, 20f),
+                                                     yNew, 0);
+        }
+    }
+}
---
+++ Lab/Assets/Scripts/FlaskScript.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlaskScript : MonoBehaviour
+{
+    Rigidbody flask;
+    public Vector3 mousePos;
+    private Vector3 prevMousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flask = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        if (prevMousePos != new Vector3())
+        {
+            mousePos = Input.mousePosition;
+            transform.position += (mousePos - prevMousePos) * Time.deltaTime * 2;
+            prevMousePos = mousePos;
+
+        //    if (Input.mouseScrollDelta != new Vector2(0, 0)) // вращение по колесику мыши
+                transform.rotation = Quaternion.Euler(0, 0, Input.mouseScrollDelta.y * 3 + transform.rotation.eulerAngles.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        prevMousePos = Input.mousePosition;
+    }
+    private void OnMouseUp()
+    {
+        prevMousePos = new Vector3();
+    }
+
+}
