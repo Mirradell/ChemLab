a4a69defac0ea5292e190f1f3dd56d9b720c7ace
Mirradell <olya.shtejn@yandex.ru>
2021-06-30 14:51:54 +0300

    improved mouse control

---
+++ Lab/Assets/Scripts/MouseFollowingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollowingScript : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private float zCoordViewport;
+    private float zCoordWorld;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        zCoordViewport = Camera.main.WorldToViewportPoint(rigidbody.position).z;
+        zCoordWorld = rigidbody.position.z;
+    }
+
+    private void OnMouseDrag()
+    {
+        var mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        var viewport = Camera.main.ViewportToWorldPoint(new Vector3(mousePos.x, mousePos.y, zCoordViewport));
+        rigidbody.position = new Vector3(viewport.x, viewport.y, zCoordWorld);
+    }
+
+    private void OnMouseDown()
+    {
+        rigidbody.useGravity = false;
+    }
+
+    private void OnMouseUp()
+    {
+        rigidbody.useGravity = true;
+    }
+}
